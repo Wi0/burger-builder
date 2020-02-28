6c114fefea68451c63a933d02d0830c180163e0a
Duncan Williams <duncanlw@gmail.com>
2020-02-28 19:49:43 +0200

    added Firebase for orders

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Spinner.module.css';
+
+const spinner = (props) => (
+    <div className={classes.ldsRoller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+);
+
+export default spinner;
