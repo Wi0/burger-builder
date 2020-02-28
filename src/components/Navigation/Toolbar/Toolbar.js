f79bdb5b64347a17c8f68cfc5a28e672239d803a
Duncan Williams <duncanlw@gmail.com>
2020-02-28 12:10:41 +0200

    Finished initial project module

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../../components/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import MenuButton from '../../UI/Button/MenuButton';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <MenuButton clicked={props.clicked} />
         <div className={classes.Logo}>
             <Logo />
         </div>
---
+++ src/components/UI/Button/MenuButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './MenuButton.module.css';
+
+const menuButton = (props) => (
+    <div
+        className={classes.DrawerToggle}
+        onClick={props.clicked}
+    >
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default menuButton;
